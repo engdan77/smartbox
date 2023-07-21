4a5c498d7f6d9dc93b9925c181057e725c390806
Daniel Engval <daniel@engvalls.eu>
2023-07-21 21:42:05 +0200

    Attempt fixing digits

--- src/mydisplay.py
+++ src/mydisplay.py
@@ -1,4 +1,5 @@
 import gc
+import time
 
 try:
     from machine import Pin, I2C
@@ -76,4 +77,70 @@ class MyDisplay:
             current_row_px += px_between_lines
         self.display.show()
 
+    def get_char_rectangles(self, char='0', x_offset=0, y_offset=0):
+        def get_digit_coords(part='upper'):
+            r = {
+                'upper': (0, 0, 20, 5),
+                'middle': (0, 20, 25, 25),
+                'lower': (0, 40, 25, 45),
+                'right_all': (20, 0, 25, 45),
+                'left_all': (0, 0, 5, 45),
+                'left_upper': (0, 0, 5, 25),
+                'right_lower': (20, 20, 25, 45),
+                'right_upper': (20, 0, 25, 25),
+                'left_lower': (0, 20, 5, 45)
+            }
+            return list(r[part])
+        digit_rects = {'0': ('upper', 'lower', 'left_all', 'right_all'),
+                       '1': ('left_all',),
+                       '2': ('upper', 'right_upper', 'middle', 'left_lower', 'lower'),
+                       '3': ('upper', 'middle', 'lower', 'right_all'),
+                       '4': ('left_upper', 'middle', 'right_all'),
+                       '5': ('upper', 'left_upper', 'middle', 'right_lower', 'lower'),
+                       '6': ('upper', 'left_all', 'middle', 'right_lower', 'lower'),
+                       '7': ('upper', 'right_all'),
+                       '8': ('upper', 'middle', 'lower', 'left_all', 'right_all'),
+                       '9': ('upper', 'middle', 'left_upper', 'right_all')}
+        all_pieces = digit_rects[char]
+        pieces_with_offsets = []
+        for piece in all_pieces:
+            coords = get_digit_coords(piece)
+            for i in (0, 2):
+                coords[i] += x_offset
+            for i in (1, 3):
+                coords[i] += y_offset
+            print(f'coord for {char}: {coords}')
+            pieces_with_offsets.append(coords)
+        return pieces_with_offsets
+
+    def draw_rect(self, coords):
+        self.display.fill(0)
+        self.display.show()
+        start_x, start_y, end_x, end_y = coords
+        self.display.fill_rect(start_x, start_y, end_x, end_y, 1)
+        self.display.show()
+
+    def draw_rects(self, coords):
+        self.display.fill(0)
+        self.display.show()
+        x_offset = 0
+        y_offset = 0
+        for rect in coords:
+            start_x, start_y, end_x, end_y = rect
+            self.display.fill_rect(start_x + x_offset, start_y + y_offset, end_x + x_offset - start_x,
+                                   end_y + y_offset - start_y, 1)
+            time.sleep(0.5)
+        self.display.show()
+
+    def draw_chars(self, chars, pixels_between_x=32):
+        self.display.fill(0)
+        self.display.show()
+        x_offset = 0
+        y_offset = 0
+        for char in str(chars):
+            for rect in self.get_char_rectangles(char, x_offset, y_offset):
+                start_x, start_y, end_x, end_y = rect
+                self.display.fill_rect(start_x, start_y, end_x, end_y, 1)
+            x_offset += pixels_between_x
+        self.display.show()
 
